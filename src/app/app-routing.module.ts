05da4e9af869c5823ed73f129287e6dc3137f85e
Edward Cabrera <93951069+WardedC@users.noreply.github.com>
2024-08-24 12:30:27 -0400

    productscomponent was created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { Routes } from '@angular/router';
 import { DashboardPageComponent } from './pages/dashboardPage/dashboardPage.component';
 import { FromsComponent } from './pages/froms/froms.component';
 import { GaleryComponent } from './pages/Galery/Galery.component';
+import { ProductsComponent } from './pages/products/products.component';
 
 export const routes: Routes = [
 
   { path: '', redirectTo: '/DashBoard', pathMatch: 'full' }, // Redirige a home por defecto
   { path: 'DashBoard', component: DashboardPageComponent },
   { path: 'Forms', component: FromsComponent },
-  { path: 'Galery', component: GaleryComponent }
+  { path: 'Galery', component: GaleryComponent },
+  { path: 'Products', component: ProductsComponent }
 
 
 ];
